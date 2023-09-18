5769e8d324e3f6392fd7a7b54ae37e5a856fcd50
jkwiatk1 <01142696@pw.edu.pl>
2023-09-18 17:37:00 +0200

    Still problem with connection. Some changes

--- populate_prices.py
+++ populate_prices.py
@@ -15,30 +15,37 @@ async def main():
         conn = sqlite3.connect(config.DB_FILE)
         cursor = conn.cursor()
         cursor.execute("SELECT id, symbol FROM stock")
-        symbols = cursor.fetchall()
+        existing_symbols = cursor.fetchall()
+        iterations_num = 0
 
-        for stock_id, symbol in symbols:
-            print(f"Processing symbol: {symbol}")
-            hist_data_collector = HistoricalDataCollector(
-                symbol = symbol,
-                start ='2000-01-01',
-                end ='2023-08-01',
-                period = PeriodCode.PERIOD_D1,
-                credentials_file = "my_secrets/credentials.json"
-            )
+        for stock_id, symbol in existing_symbols:
+            iterations_num += 1
+            print(f"{iterations_num}. Processing symbol: {symbol}")
 
-            hist_data_df = await hist_data_collector.run()
+            cursor.execute("SELECT COUNT(*) FROM stock_price_1d WHERE stock_id = ?", (stock_id,))
+            count = cursor.fetchone()[0]
 
-            for date_index, row in hist_data_df.iterrows():
-                cursor.execute("""
-                    INSERT INTO stock_price_1d (stock_id, date, open, close, high, low, volume)
-                    VALUES (?, ?, ?, ?, ?, ?, ?)
-                """, [stock_id, date_index.strftime('%Y-%m-%d %H:%M:%S'), row['Open'], row['Close'], row['High'], row['Low'], row['Volume']])
+            if count == 0:
+                hist_data_collector = HistoricalDataCollector(
+                    symbol = symbol,
+                    start ='2000-01-01',
+                    end ='2023-08-01',
+                    period = PeriodCode.PERIOD_D1,
+                    credentials_file = "my_secrets/credentials.json"
+                )
 
-            del hist_data_collector
-            gc.collect()
+                hist_data_df = await hist_data_collector.run()
 
-        conn.commit()
+                for date_index, row in hist_data_df.iterrows():
+                    cursor.execute("""
+                        INSERT INTO stock_price_1d (stock_id, date, open, close, high, low, volume)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)
+                    """, [stock_id, date_index.strftime('%Y-%m-%d %H:%M:%S'), row['Open'], row['Close'], row['High'], row['Low'], row['Volume']])
+
+                del hist_data_collector
+                gc.collect()
+                conn.commit()
+
         conn.close()
 
     except xapi.LoginFailed as e:
