bfba6c598b1ad6d98b25415ea2a50f163d8e95ae
jkwiatk1 <01142696@pw.edu.pl>
2023-09-21 15:43:50 +0200

    Init class to analyse historical data

---
+++ scripts/StockDataAnalyzer.py
@@ -0,0 +1,23 @@
+from HistoricalDataCollector import HistoricalDataCollector
+
+class StockDataAnalyzer:
+    def __init__(self, data):
+        self.data = data
+
+    def get_max_value_in_range(self, start_date, end_date):
+        pass
+
+    def get_max_value_in_months(self):
+        pass
+
+    def get_max_value_in_year(self):
+        pass
+
+    def calculate_average(self):
+        pass
+
+    def calculate_volatility(self):
+        pass
+
+    def generate_report(self):
+        pass
