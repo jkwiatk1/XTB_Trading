85fd9492d656c63767007f7538013937c409e65b
jkwiatk1 <01142696@pw.edu.pl>
2023-09-28 16:47:44 +0200

    Add button to download newest data if needed. TODO modify HistoricalDataCollector to download newest data, now it download data if they are not stock_id in db.

--- web_app/app.py
+++ web_app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from database_operations.DatabaseManager import DatabaseManager
 from my_secrets import config
@@ -57,3 +57,30 @@ async def stock_detail(request: Request, symbol):
 
 
     return templates.TemplateResponse("stock_detail.html",{"request": request, "stock": company[0], "bars": prices})
+
+
+@app.post("/stock/{symbol}/download")
+async def download_newest_data(request: Request, symbol: str, symbol_id: int = Form(...)):
+    database_conn = DatabaseManager(config.DB_FILE)
+    await database_conn.connect_to_db()
+    company = await database_conn.get_specify_data("stock",["id","symbol", "name"], f"symbol = ?",(symbol,))
+
+    print(f"Newest data for symbol: {symbol} will be downloaded.")
+    symbol = symbol
+    symbol_id = symbol_id
+
+    hist_data_collector = HistoricalDataCollector(
+        symbol= symbol,
+        start='2000-01-01',
+        end=datetime.today().strftime('%Y-%m-%d'),
+        period=PeriodCode.PERIOD_D1,
+        credentials_file=config.CREDENTIALS_PATH
+    )
+    await hist_data_collector.connect_to_xapi()
+    await database_conn.populate_prices("stock_price_1d", symbol_id, symbol, hist_data_collector)
+    print("Successfully downloaded.")
+
+    prices = await database_conn.get_ordered_data("stock_price_1d",columns = ["*"], order_by_column = "date",conditions= f"stock_id = ?", condition_params =  (company[0]['id'],), ascending=False)
+
+
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": company[0], "bars": prices})
